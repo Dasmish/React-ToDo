bf40c8229c4388901f4a9f955b647f9dc5a089de
Dasmish <kosty.filipenko@gmail.com>
2019-11-07 17:48:46 +0200

    LocalStorage Update

--- src/Todo_Comp.jsx
+++ src/Todo_Comp.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import axios from 'axios'
 
-let storage = window.localStorage;
+import TaskList from './tasks.json'
+
+const storage = window.localStorage;
 let counter = null;
+let ReactTodos = [];
+let ReactTodo = {};
+
 export default class TodoList extends React.PureComponent {
 
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            lists: [],
+        }
     }
 
-    createLi = (InnerText) => {
+    createLi = InnerText => {
         let node = document.createElement('li');
-        let div_node = document.createElement('div');
-        div_node.className = 'delete-task';
+        // let count_node = document.createElement('div');
+        let delete_node = document.createElement('div');
+        // count_node.textContent = id;
+        delete_node.className = 'delete-task';
         let text = document.createTextNode(InnerText)
-        node.appendChild(div_node)
+        // node.appendChild(count_node)
+        node.appendChild(delete_node)
         node.appendChild(text);
         document.querySelector('.ul-list').appendChild(node)
     }
@@ -22,9 +33,18 @@ export default class TodoList extends React.PureComponent {
     createList = (e) => {
         let list = document.querySelector('.todo-input')
         if(e.code === 'Enter') {
-            counter = counter + 1;
-            console.log(typeof counter)
-            storage.setItem('task' + counter, list.value)
+            counter += 1;
+            const ReactStorage = JSON.parse(storage.getItem("React Tasks"));
+            if(ReactStorage != null) {
+                
+            }
+            ReactTodo = {
+                id: counter,
+                value: list.value
+            }
+            ReactTodos.push(ReactTodo);
+            console.log(ReactTodos);
+            storage.setItem('React Tasks', JSON.stringify(ReactTodos));
             this.createLi(list.value);
             list.value = '';
             
@@ -32,33 +52,53 @@ export default class TodoList extends React.PureComponent {
         }
     }
 
+    onDeleteTask = (index) => {
+        console.log(index)
+    }
+
     getTasks = () => {
         counter = +(storage.getItem('counter'));
-        for (let index = 1; index <= storage.length; index++) {
-            if(storage.getItem('task' + index) !== null) {
-                let store_text = storage.getItem('task' + index)
-                this.createLi(store_text);
+        let ReactStorage = JSON.parse(storage.getItem("React Tasks"));
+        if(ReactStorage != null) {            
+            for (let index = 0; index < ReactStorage.length; index++) {
+                console.log(ReactStorage[index].value)
+                this.createLi(ReactStorage[index].value)
             }
         }
     }
 
     getJSONTasks () {
-        fetch('/tasks.json').then(
-            resp => resp.json()
-        ).then(
-            data => {
-                console.log(data)
-                
-            }
-        )
+        try {
+            TaskList.forEach(item => {
+                this.createLi(item.text)
+            })
+        } catch (error) {
+            alert(error)
+        }
+    }
+    setJSONTask = (e) => {
+        if(e.code === 'Enter') {
+            let val = document.querySelector('input');
+            this.createLi(val.value);
+            let rawData = JSON.stringify({id: 4, text: val.value})
+            let blob = new Blob([rawData], {type : 'application/json'})
+            console.log(blob)
+            val.value = '';
+        }
     }
 
     componentDidMount() {
-        // this.getTasks();
-        this.getJSONTasks();
+        this.getTasks();
+        // this.getJSONTasks();
 
         document.querySelector('.todo-input').addEventListener('keydown', e => {
+            // this.setJSONTask(e);
             this.createList(e);
+        })
+        document.addEventListener('click', e => {
+            if(e.target.className === 'delete-task') {
+                this.onDeleteTask(e.target)
+            }
         })
     }
 
